1b4aaaa5218246a4621c4bb649e48acbc7a62ff7
Boris <sslonovborisss@gmail.com>
2022-09-18 07:06:13 +0300

    ready

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -22,6 +22,20 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+//smooth scroll
+const anchors = document.querySelectorAll(".header__link");
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
 //Hidden header
 let header = document.querySelector(".header");
 let scrollPrev = 0;
@@ -44,7 +58,6 @@ window.addEventListener("scroll", function () {
 });
 
 window.onload = function () {
-  location.href = "#";
   Splitting();
   ScrollOut({
     targets: ".word",
